b899c7c243c860f4348048a50962b6e49ace3436
Loïc Girault <lgirault@mediarithmics.com>
2022-11-17 22:53:32 +0100

    subscription struct and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,28 @@
 //! src/lib.rs
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
-
-async fn health_check () -> impl Responder {
-    HttpResponse::Ok()
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
 }
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
 
-    format!("Hello {name}!")
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
-            .route("/{name}", web::get().to(greet))
+            .route("/subscriptions", web::post().to(subscribe))
     })
-        .listen(listener)?
-        .run();
+    .listen(listener)?
+    .run();
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-
 use std::net::TcpListener;
 use zero2prod::run;
 
 //cargo +nightly expand
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-        let listener = TcpListener::bind("127.0.0.1:8000")?;
-        run(listener)?.await
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
