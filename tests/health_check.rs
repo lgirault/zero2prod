48e26337f9e5727c423ecafea94f135574f255c3
Loïc Girault <lgirault@mediarithmics.com>
2022-11-18 22:09:57 +0100

    split modules, add db usage test, update github action

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,8 @@
 //! tests/health_check.rs
 
 use std::net::TcpListener;
+use sqlx::{PgConnection, Connection};
+use zero2prod::configuration::get_configuration;
 
 //cargo expand --test health_check
 #[tokio::test]
@@ -22,6 +24,11 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let host = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+
+    let mut connection = PgConnection::connect(&connection_string)
+        .await.expect("Failed to connect to Postgres");
 
     let client = reqwest::Client::new();
 
@@ -36,6 +43,13 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
@@ -71,7 +85,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     tokio::spawn(server);
     format!("http://127.0.0.1:{port}")
 }
