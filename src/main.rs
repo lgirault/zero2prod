c7ef0902e5b38f280edb715a46a9517acc8b79d9
Loïc Girault <lgirault@mediarithmics.com>
2022-11-21 21:54:21 +0100

    Add tracing

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use env_logger::Env;
 
 //cargo +nightly expand
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection = PgPool::connect(&configuration.database.connection_string())
         .await
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,7 +9,16 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(form: web::Form<FormData>, connection: web::Data<PgPool>) -> HttpResponse {
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    tracing::info!("request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+            form.email,
+            form.name);
+
+    tracing::info!("request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -20,12 +29,17 @@ pub async fn subscribe(form: web::Form<FormData>, connection: web::Data<PgPool>)
         form.name,
         Utc::now()
     )
-    .execute(connection.get_ref())
-    .await
+        .execute(pool.get_ref())
+        .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("request_id {} - New subscriber details have been saved",
+        request_id);
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {e}");
+            tracing::error!("request_id {} - Failed to execute query: {:?}",
+        request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
