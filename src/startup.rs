a461c4a9f36513bf8d10732c12306aaccc458b24
Loïc Girault <lgirault@mediarithmics.com>
2022-12-26 23:04:38 +0100

    about to remove hmac

--- src/startup.rs
+++ src/startup.rs
@@ -1,11 +1,12 @@
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, publish_newsletter, subscribe};
+use crate::routes::*;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use secrecy::Secret;
 use tracing_actix_web::TracingLogger;
 
 pub struct Application {
@@ -57,6 +58,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
@@ -84,6 +86,7 @@ pub fn run(
     connection: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> std::io::Result<Server> {
     let connection = web::Data::new(connection); //Atomic Reference Counted pointer
     let email_client = web::Data::new(email_client);
@@ -95,11 +98,18 @@ pub fn run(
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
+            .route("/", web::get().to(home))
+            .route("/login", web::get().to(login_form))
+            .route("/login", web::post().to(login))
             .app_data(connection.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(web::Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
     Ok(server)
 }
+
+#[derive(Clone)]
+pub struct HmacSecret(pub Secret<String>);
